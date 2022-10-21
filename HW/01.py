52a2776fae3087cabee0e2882dcd9929d84943c8
«iliya <iliya.91f@gmail.com>
2022-10-21 18:01:05 +0300

    HW1

---
+++ HW/01.py
@@ -0,0 +1,19 @@
+
+day = int (input('Please enter the number of the weekday: '))
+if day > 0 and day <= 5:
+    print('Working day ')
+elif day >= 6 and day <= 7:
+    print('Weekend!')
+else:
+    print('Please enter the number from 1 to 7')
+
+
+
+
+
+# Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+# Пример:
+# - 6 -> да
+# - 7 -> да
+# - 1 -> нет
+#
