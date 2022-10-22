c501af0ed982cf18992a697413f6e8a7ec81cc71
«iliya <iliya.91f@gmail.com>
2022-10-22 14:43:46 +0300

    HW01

--- HW/03.py
+++ HW/03.py
@@ -1,10 +1,31 @@
+
+def Input_Coordinates():
+    x, y = input('Please enter coordinates x , y: ') .split(",")
+    while int(x) == 0 or int(y) == 0:
+        print("Coordinates can't be equal 0. Please enter again")
+        x, y = input('Please enter coordinates x , y: ') .split(",")
+    else:
+      return int (x), int(y)
+
+def CheckCoordinates (x,y):
+    if x > 0 and y > 0:
+     print('Quarter 1')
+    elif x < 0 and y > 0:
+      print('Quarter 2')
+    elif x < 0 and y < 0:
+     print('Quarter 3')
+    elif x > 0 and y < 0:
+      print('Quarter 4')
+
+
+x, y = Input_Coordinates()
+print('Coordinate x:', x, 'and coordinate y:',y)
+CheckCoordinates(x,y)
+
 # Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка (или на какой оси она находится).
 # Пример:
 # - x=34; y=-30 -> 4
 # - x=2; y=4-> 1
 # - x=-34; y=-30 -> 3
-# Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
-# Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
-# Пример:
-# - A (3,6); B (2,1) -> 5,10
-# - A (7,-5); B (1,-1) -> 7,21
+
+
---
+++ HW/05.py
@@ -0,0 +1,29 @@
+
+def Input_Coordinates():
+    x, y = input('Please enter coordinates x , y: ') .split(",")
+    a = []
+    while int(x) == 0 or int(y) == 0:
+        print("Coordinates can't be equal 0. Please enter again")
+        x, y = input('Please enter coordinates x , y: ') .split(",")
+    else:
+        a.append(int(x))
+        a.append(int(y))
+        return a
+
+def DistanceBTWCoordinates (a,b):
+    length = ((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2) ** (0.5)
+    return int(length)
+
+
+a = Input_Coordinates()
+b = Input_Coordinates()
+length = DistanceBTWCoordinates(a,b)
+
+print(length)
+
+
+# Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+# Пример:
+# - A (3,6); B (2,1) -> 5,10
+# - A (7,-5); B (1,-1) -> 7,2
+
