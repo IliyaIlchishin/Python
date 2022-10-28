94ade3654e64d3dc2cb8ef8b0975d396bfb3422d
«iliya <iliya.91f@gmail.com>
2022-10-28 04:46:20 +0300

    HW04

--- HW04/03.py
+++ HW04/03.py
@@ -1,8 +1,20 @@
 
-numbers = [int(i) for i in input('Введите номера через запятую ').split(',')]
-lst = set(numbers)
-print(lst)
+# numbers = [int(i) for i in input('Введите номера через запятую ').split(',')]
+# lst = set(numbers)
+# print(lst)
 
-# Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+
+
+# Задайте последовательность чисел. 
+# Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
 # 3 1 2 3
 # 2 1
+
+
+# numbers = [int(i) for i in input('Введите номера через запятую ').split(',')]
+# print(numbers)
+# unique = []
+# for i in numbers:
+#     if numbers.count(i) <2:
+#         unique.append(i)
+# print(unique)
--- HW04/04.py
+++ HW04/04.py
@@ -1,4 +1,20 @@
 
+from random import randint
+
+k = int(input ('Введите степень к: '))
+
+for i in range(k, 0, -1):
+    koef = randint (1, 101)
+    if koef == 1:
+        koef = ''
+    else:
+        if i != 1:
+          koef = f'{koef}*x^{i}+'
+        else:
+            koef = f'{koef} * x + '
+    print(koef, end=' ' )
+print (f' {randint (1, 101)} = 0')
+
 
 # Задана натуральная степень k. 
 # Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и вывести многочлен степени k.
