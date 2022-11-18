229140e79f9d84be716634729c90c116c4d1699d
«iliya <iliya.91f@gmail.com>
2022-11-18 11:04:32 +0300

    HW

--- HW07/phone book/DBimport.py
+++ HW07/phone book/DBimport.py
@@ -1,15 +1,15 @@
 
 
-def importPhoneBook():
-    importlist = []
-    with open("export.txt", "r") as file:
-        for i in file:
-            importlist.append(i)
-            importstr = str(importlist)
-            importstr.remove('Фамилия - ')
+# def importPhoneBook():
+#     importlist = []
+#     with open("export.txt", "r") as file:
+#         for i in file:
+#             importlist.append(i)
+#             importstr = str(importlist)
+#             importstr.remove('Фамилия - ')
 
-    return importstr
+#     return importstr
 
-importlist = importPhoneBook()
-print(importlist)
+# importlist = importPhoneBook()
+# print(importlist)
 
